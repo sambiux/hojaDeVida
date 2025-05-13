fef620d817ce7d88750d3c43454d5e1fe48ef6d0
sambiux <sddaza.9@gmail.com>
2025-05-13 18:49:13 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+const imagenSam= document.querySelector(".btnInformacion");
+
+imagenSam.addEventListener("click", (event) =>{
+    event.preventDefault();
+
+    window.location.href= "indexInformacionSam.html";
+})
